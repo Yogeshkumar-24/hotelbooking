9e5dfe09f28233c0824dfd07018b0ebf3e0aa16f
Yogeshkumar-24 <YOGESHKUMAR_R@outlook.com>
2023-02-21 00:32:13 +0530

    second commit

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -21,7 +21,7 @@ const List = () => {
   const[max,setMax] = useState(undefined)
 
   const { data, loading, error, reFetch } = useFetch(
-    `/hotels?city=${destination}&min=${min || 0}&max=${max || 9999}`
+    `https://hotel-server-production.up.railway.app/api/hotels?city=${destination}&min=${min || 0}&max=${max || 9999}`
   );
    
   const handleClick = () => {

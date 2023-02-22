5d204b88ebe7eb344d389124c42d6479d30cfccd
Yogeshkumar-24 <YOGESHKUMAR_R@outlook.com>
2023-02-22 20:13:09 +0530

    updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -87,7 +87,9 @@ export const Header = ({type}) => {
               type="text"
               placeholder="Enter Place"
               className="headerSearchInput"
-              onChange={(e) => setDestination(e.target.value.toLowerCase())}
+              onChange={(e) => setDestination(() => {
+                e.target.value = e.target.value.toLowerCase()
+              })}
             />
           </div>
           <div className="headerSearchItem">
